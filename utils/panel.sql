dc2a330c21d51e41366713e9fce910222f32c9d2
Romain Westeel <romainwesteel@orange.fr>
2023-08-22 23:26:26 +0200

    Change sql

--- utils/panel.sql
+++ utils/panel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 22, 2023 at 03:32 PM
+-- Generation Time: Aug 22, 2023 at 11:25 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -77,8 +77,7 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL
+  `prenom` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
