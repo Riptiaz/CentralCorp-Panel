743f03a24f3a5bc080489829657fa7ccd692b5a7
Romain Westeel <romainwesteel@orange.fr>
2023-09-05 17:43:53 +0200

    Update panel.sql

--- utils/panel.sql
+++ utils/panel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2023 at 03:58 PM
+-- Generation Time: Sep 05, 2023 at 05:41 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -41,20 +41,20 @@ CREATE TABLE `ignored_folders` (
 CREATE TABLE `options` (
   `id` int(11) NOT NULL,
   `maintenance` tinyint(1) NOT NULL,
-  `maintenance_message` text,
-  `minecraft_version` varchar(50),
+  `maintenance_message` text DEFAULT NULL,
+  `minecraft_version` varchar(50) DEFAULT NULL,
   `mods_enabled` tinyint(1) NOT NULL,
   `file_verification` tinyint(1) NOT NULL,
   `embedded_java` tinyint(1) NOT NULL,
-  `game_folder_name` varchar(100),
-  `server_name` varchar(100),
-  `server_ip` varchar(50),
-  `server_port` int(11),
-  `loader_type` varchar(50),
-  `loader_build_version` varchar(50),
-  `loader_activation` varchar(10),
-  `changelog_version` varchar(20),
-  `changelog_message` text,
+  `game_folder_name` varchar(100) DEFAULT NULL,
+  `server_name` varchar(100) DEFAULT NULL,
+  `server_ip` varchar(50) DEFAULT NULL,
+  `server_port` int(11) DEFAULT NULL,
+  `loader_type` varchar(50) DEFAULT NULL,
+  `loader_build_version` varchar(50) DEFAULT NULL,
+  `loader_activation` varchar(10) DEFAULT NULL,
+  `changelog_version` varchar(20) DEFAULT NULL,
+  `changelog_message` text DEFAULT NULL,
   `role` int(11) NOT NULL,
   `money` int(11) NOT NULL,
   `server_img` text NOT NULL,
@@ -79,7 +79,7 @@ CREATE TABLE `options` (
 --
 
 INSERT INTO `options` (`id`, `maintenance`, `maintenance_message`, `minecraft_version`, `mods_enabled`, `file_verification`, `embedded_java`, `game_folder_name`, `server_name`, `server_ip`, `server_port`, `loader_type`, `loader_build_version`, `loader_activation`, `changelog_version`, `changelog_message`, `role`, `money`, `server_img`, `splash`, `splash_author`, `azuriom`, `ftp_url`, `rpc_activation`, `rpc_id`, `rpc_details`, `rpc_state`, `rpc_large_text`, `rpc_small_text`, `rpc_button1`, `rpc_button1_url`, `rpc_button2`, `rpc_button2_url`) VALUES
-(1, 0, 'Le launcher est en maintenance, merci de relancer ultérieurement.', '1.19.3', 1, 1, 1, 'centralcorp', 'yourservername', 'yourservername.com', 25565, 'forge', '1.19.3-44.1.0', '1', '1.0.0', 'Derniere version du launcher', 0, 0, 'https://conflictura.site/storage/img/logo2.png', 'Ceci est du code', 'Riptiaz', 'https://conflictura.site', 'http://129.151.251.180', 0, '1144257170561581097', 'Dans le launcher', 'En exploration', 'Minecraft', 'Multiplayer server', 'Discord', 'https://discord.gg/VCmNXHvf77', 'Site Web', 'https://conflictura.site');
+(1, 0, 'Le launcher est en maintenance, merci de relancer ultérieurement.', '1.19.3', 1, 1, 1, 'centralcorp', 'yourservername', 'yourservername.com', 25565, 'forge', '1.19.3-44.1.0', '1', '1.0.0', 'Derniere version du launcher', 0, 0, 'https://conflictura.site/storage/img/logo2.png', 'Ceci est du code', 'Riptiaz', 'https://conflictura.site', 'http://129.151.251.180', 1, '1144257170561581097', 'Dans le launcher', 'En exploration', 'Minecraft', 'Multiplayer server', 'Discord', 'https://discord.gg/VCmNXHvf77', 'Site Web', 'https://conflictura.site');
 
 -- --------------------------------------------------------
 
