761e09482676f25b6a4f5910d059660dd20e6bff
Romain <romainwesteel@orange.fr>
2025-03-17 16:58:37 +0100

    v1.5

--- function/loader_api.php
+++ function/loader_api.php
@@ -11,17 +11,18 @@ function getForgeBuilds($mcVersion) {
         $dom->loadHTML($html);
         libxml_clear_errors();
         $xpath = new DOMXPath($dom);
-        $versionsNodeList = $xpath->query('//td[@class="download-version"]');
 
-        foreach ($versionsNodeList as $versionNode) {
-            $version = trim($versionNode->nodeValue);
-            if (!empty($version)) {
-                $builds[] = "$mcVersion-$version";
+        $links = $xpath->query('//a[contains(@href, "maven.minecraftforge.net/net/minecraftforge/forge/")]');
+
+        foreach ($links as $link) {
+            $href = $link->getAttribute('href');
+            if (preg_match('/forge\/([\d.]+-[\d.]+-[\d.]+)/', $href, $matches)) {
+                $builds[] = $matches[1];
             }
         }
     }
 
-    return $builds;
+    return array_unique($builds);
 }
 
 if (isset($_GET['loader']) && isset($_GET['mc_version'])) {
