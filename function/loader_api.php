16ab8146058c812b62258f48eb6b1099313f2bfb
Romain <romainwesteel@orange.fr>
2025-03-17 17:20:50 +0100

    v.1.5.2

--- function/loader_api.php
+++ function/loader_api.php
@@ -1,42 +1,39 @@
 <?php
 function getForgeBuilds($mcVersion) {
     $url = "https://files.minecraftforge.net/net/minecraftforge/forge/index_$mcVersion.html";
-    $html = @file_get_contents($url);
-
-    if (!$html) {
-        return [];
-    }
-
-    $dom = new DOMDocument;
-    libxml_use_internal_errors(true);
-    $dom->loadHTML($html);
-    libxml_clear_errors();
-    $xpath = new DOMXPath($dom);
     $builds = [];
 
-    $versionsNodeList = $xpath->query('//td[@class="download-version"]');
-    if ($versionsNodeList->length > 0) {
-        foreach ($versionsNodeList as $versionNode) {
-            $version = trim($versionNode->nodeValue);
-            if (!empty($version)) {
-                $builds[] = "$mcVersion-$version";
-            }
-        }
-    }
+    // Récupérer le contenu HTML de la page
+    $html = @file_get_contents($url);
 
-    $links = $xpath->query('//a[contains(@href, "maven.minecraftforge.net/net/minecraftforge/forge/")]');
-    if ($links->length > 0) {
+    if ($html !== false) {
+        $dom = new DOMDocument;
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($html);
+        libxml_clear_errors();
+        $xpath = new DOMXPath($dom);
+
+        // Sélectionner les liens contenant "maven.minecraftforge.net/net/minecraftforge/forge/"
+        $links = $xpath->query('//a[contains(@href, "maven.minecraftforge.net/net/minecraftforge/forge/")]');
+
         foreach ($links as $link) {
             $href = $link->getAttribute('href');
-            if (preg_match('/forge\/([\d.]+-[\d.]+-[\d.]+)/', $href, $matches)) {
-                $builds[] = $matches[1];
+
+            // Extraire la version depuis l'URL
+            if (preg_match('/forge\/([\d\.\-]+)\/forge-\1-/', $href, $matches)) {
+                $version = $matches[1];
+
+                if (!in_array($version, $builds)) {
+                    $builds[] = $version;
+                }
             }
         }
     }
 
-    return array_unique($builds);
+    return $builds;
 }
 
+// Vérification des paramètres GET
 if (isset($_GET['loader']) && isset($_GET['mc_version'])) {
     $loader = $_GET['loader'];
     $mcVersion = $_GET['mc_version'];
