d047c37024fea71514e2d33efba604c30a58ae24
Romain <romainwesteel@orange.fr>
2025-03-17 17:02:08 +0100

    v1.5.1

--- function/loader_api.php
+++ function/loader_api.php
@@ -1,19 +1,31 @@
 <?php
-
 function getForgeBuilds($mcVersion) {
     $url = "https://files.minecraftforge.net/net/minecraftforge/forge/index_$mcVersion.html";
-    $html = file_get_contents($url);
+    $html = @file_get_contents($url);
+
+    if (!$html) {
+        return [];
+    }
+
+    $dom = new DOMDocument;
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($html);
+    libxml_clear_errors();
+    $xpath = new DOMXPath($dom);
     $builds = [];
 
-    if ($html) {
-        $dom = new DOMDocument;
-        libxml_use_internal_errors(true);
-        $dom->loadHTML($html);
-        libxml_clear_errors();
-        $xpath = new DOMXPath($dom);
+    $versionsNodeList = $xpath->query('//td[@class="download-version"]');
+    if ($versionsNodeList->length > 0) {
+        foreach ($versionsNodeList as $versionNode) {
+            $version = trim($versionNode->nodeValue);
+            if (!empty($version)) {
+                $builds[] = "$mcVersion-$version";
+            }
+        }
+    }
 
-        $links = $xpath->query('//a[contains(@href, "maven.minecraftforge.net/net/minecraftforge/forge/")]');
-
+    $links = $xpath->query('//a[contains(@href, "maven.minecraftforge.net/net/minecraftforge/forge/")]');
+    if ($links->length > 0) {
         foreach ($links as $link) {
             $href = $link->getAttribute('href');
             if (preg_match('/forge\/([\d.]+-[\d.]+-[\d.]+)/', $href, $matches)) {
@@ -37,4 +49,5 @@ if (isset($_GET['loader']) && isset($_GET['mc_version'])) {
     header('Content-Type: application/json');
     echo json_encode(['builds' => $builds]);
 }
+
 ?>
